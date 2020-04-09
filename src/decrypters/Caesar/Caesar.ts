48f41aa1d75e157ffbf40e7d520ba4b20751214d
lalitohg <eduardohdzgro@gmail.com>
2020-04-08 23:43:13 -0500

    added caesar cipher

--- src/decrypters/Caesar/Caesar.ts
+++ src/decrypters/Caesar/Caesar.ts
@@ -1,27 +1,49 @@
 import { defaultAlphabet } from '../common'
 
+const chooseToken = (index: number, alphabet: string[]): string => (index < alphabet.length - 3 ? alphabet[index + 3] : alphabet[index - (alphabet.length - 3)]);
+
+const encryptMapReducer: any = (reducer: any, token: string, index: number, alphabet: string[]) => ({
+    ...reducer,
+    [token]: chooseToken(index, alphabet)
+});
+
+const decryptMapReducer: any = (reducer: any, token: string, index: number, alphabet: string[]) => ({
+    ...reducer,
+    [chooseToken(index, alphabet)]: token
+});
+
 export class Caesar {
     private _encryptMap: any;
     private _decryptMap: any;
     private _alphabet: string[];
 
-    constructor(alphabet = defaultAlphabet) {
-        this._alphabet = alphabet;
-        this._generateMappings();
+    constructor(alphabet: string[] = defaultAlphabet) {
+        this._alphabet = alphabet.length > 3 ? alphabet.map(token => token.toLowerCase()) : defaultAlphabet;
+        this._encryptMap = this._alphabet.reduce(encryptMapReducer, {});
+        this._decryptMap = this._alphabet.reduce(decryptMapReducer, {});
     }
 
-    get alphabet():string[] {
+    get alphabet(): string[] {
         return [...this._alphabet];
     }
     get encryptMap() {
-        return {...this._encryptMap};
+        return { ...this._encryptMap };
     }
     get decryptMap() {
-        return {...this._decryptMap};
+        return { ...this._decryptMap };
     }
 
-    private _generateMappings() {
-
+    public encryptString(message: string): string {
+        return message.toLowerCase().split('').reduce(
+            (reducer: string, token: string) => this.encryptMap[token] ? reducer += this.encryptMap[token] : reducer += token,
+            ''
+        );
     }
-    
+
+    public decryptString(message: string): string {
+        return message.toLowerCase().split('').reduce(
+            (reducer: string, token: string) => this.decryptMap[token] ? reducer += this.decryptMap[token] : reducer += token,
+            ''
+        );
+    }
 }
