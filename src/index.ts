e2ac28cc11df2bef0a5c8367e446bd76ec042179
lalitohg <eduardohdzgro@gmail.com>
2020-05-12 00:53:25 -0500

    Add index page to test cyphers

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { Caesar } from './decrypters/Caesar';
 import { Atbash } from './decrypters/Atbash';
 import { A1Z26 } from './decrypters/A1Z26';
+import { Viginere } from './decrypters/Viginere';
 
-console.log('creating Caesar cipher');
-const caesarCipher = new Caesar();
-console.log('Created cipher with alphabet', caesarCipher.alphabet);
-
-console.log('creating Atbash cipher');
-const atbashCipher = new Atbash();
-console.log('Created cipher with alphabet', atbashCipher.alphabet);
-
-console.log('creating A1Z26 cipher');
-const a1z26Cipher = new A1Z26();
-console.log('Created cipher with alphabet', atbashCipher.alphabet);
+export {
+    Caesar,
+    Atbash,
+    A1Z26,
+    Viginere
+}
